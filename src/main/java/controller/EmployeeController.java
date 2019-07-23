6ee8d1e52d43f237bb5c97a856437fb052077634
Xxtractz <musambaloyi@gmail.com>
2019-07-23 13:31:08 +0200

    update

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import model.Employee;
+import view.EmployeeDashboardView;
+
 public class EmployeeController {
+    private Employee employeeModel;
+    private EmployeeDashboardView view;
+
+    public EmployeeController(Employee employeeModel, EmployeeDashboardView view) {
+        this.employeeModel = employeeModel;
+        this.view = view;
+    }
+
+    public void setEmployeeModel(String name, String lname){
+        employeeModel.setFirstname(name);
+        employeeModel.setLastname(lname);
+    }
+
+    public String getEmployeeName(){
+        return employeeModel.getFirstname() + " " + employeeModel.getLastname();
+    }
+
+    public void setSSN(String ssn){
+        employeeModel.setSsNumber(ssn);
+    }
+
+    public  String getSSN(){
+        return employeeModel.getSsNumber();
+    }
+
+    // Update View
+    public void updateDashboard(){
+        view.printEmployeeInformation(employeeModel);
+    }
 }
+
